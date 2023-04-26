42ca54e527c68225cb247a966f3d9a3652beffd1
Matshiro <Daniel.B.Lewinski@gmail.com>
2023-04-26 14:42:03 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Restaurant Page',
+        }),
+
+    ],
+    optimization: {
+        runtimeChunk: 'single',
+      },
+    watch: true,
+    watchOptions: {
+        ignored: /node_modules/,
+        aggregateTimeout: 1000,
+    },
+    module: {
+        rules:[
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpg|jpeg|svg|webp|gif)$/i,
+                type: 'asset/resource',
+
+            },
+        ],
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: 'images/[name][ext]',
+        clean: true,
+    },
+};
